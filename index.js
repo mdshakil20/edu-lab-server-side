0d3d7615d2b06aaf923871eb8842ecbbe919e487
Md. Shamiul Islam Shakil <75733493+mdshakil20@users.noreply.github.com>
2022-10-25 08:38:18 +0600

    index.js file created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+const categories =  require('./courses.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API running........ ');
+});
+
+
+app.listen(port, () =>{
+    console.log('Server running ')
+})
