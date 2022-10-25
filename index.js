6a3e3e39e8b622ec6de20b3eb7290f4d2aa6f5b3
Md. Shamiul Islam Shakil <75733493+mdshakil20@users.noreply.github.com>
2022-10-25 08:40:00 +0600

    port assign on 6000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = 6000;
 
 const categories =  require('./courses.json');
 
